da4bfb643d1fdd54d7a1885d254f53eef29f3cbd
Radik Abubjazov <abubjazov@mail.ru>
2022-01-17 17:23:39 +0300

    proj: add components/Htag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import { Htag } from '../components'
+
 export default function Home(): JSX.Element {
   return (
     <div>
-        Noto Sans Display
+        <Htag tag='h1'><span>Some</span> text</Htag>
+        <Htag tag='h2'><span>Some</span> text</Htag>
+        <Htag tag='h3'><span>Some</span> text</Htag>
     </div>
   )
 }
