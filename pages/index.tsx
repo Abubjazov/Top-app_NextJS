7c9ba84328f4c05d6dfda50518a9883297c1d765
Radik Abubjazov <abubjazov@mail.ru>
2022-01-17 20:50:30 +0300

    proj: add components/Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import { Htag } from '../components'
+import { Button, Htag } from '../components'
 
 export default function Home(): JSX.Element {
   return (
-    <div>
+    <>
         <Htag tag='h1'><span>Some</span> text</Htag>
         <Htag tag='h2'><span>Some</span> text</Htag>
         <Htag tag='h3'><span>Some</span> text</Htag>
-    </div>
+        <Button appearance='primary' className='arr13'>Primary</Button>
+        <Button appearance='ghost'>Ghost</Button>
+    </>
   )
 }
