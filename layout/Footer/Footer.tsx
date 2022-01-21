53c511c4e331151c5f86828b3e06d876647b3055
Radik Abubjazov <abubjazov@mail.ru>
2022-01-21 19:59:05 +0300

    layout/: Footer add content

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,10 +1,15 @@
 import cn from 'classnames'
+import { format } from 'date-fns'
 
-import {FooterProps } from './Footer.props'
+import { FooterProps } from './Footer.props'
 import styles from './Footer.module.css'
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props }: FooterProps): JSX.Element => {
 	return (
-		<div {...props}>Footer</div>
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+			<a href="http://" target="_blank" rel="noopener noreferrer">Пользовательское соглашение</a>
+			<a href="http://" target="_blank" rel="noopener noreferrer">Политика конфиденциальности</a>
+		</footer>
 	)
 }
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -9,15 +9,13 @@ import { Footer } from './Footer/Footer'
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
-		<>
-		<Header />
-		<div>
-			<Sidebar />
-			<div>
+		<div className={styles.wrapper}>
+			<Header className={styles.header}/>
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>
 				{children}
 			</div>
+			<Footer className={styles.footer}/>
 		</div>
-		<Footer />
-		</>
 	)
 }
