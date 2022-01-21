933e74b8bdb291bd6f22181b0e9bec155443bae7
Radik Abubjazov <abubjazov@mail.ru>
2022-01-21 12:17:16 +0300

    proj: add HOC/withLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
 import { Button, Htag, Ptag, Rating, Tag } from '../components'
-import { Layout } from '../layout/Layout'
+import { withLayout } from '../HOC/withLayout'
 
-export default function Home(): JSX.Element {
+
+
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(0)
 
   return (
-    <Layout>
+    <>
         <Htag tag='h1'><span>{rating}</span> text</Htag>
         <Htag tag='h2'><span>Some</span> text</Htag>
         <Htag tag='h3'><span>Some</span> text</Htag>
@@ -24,6 +26,8 @@ export default function Home(): JSX.Element {
         <Tag size='s' color='green'>Text</Tag>
         <Tag size='s' color='red'>Text</Tag>
         <Rating rating={rating} isEditable={true} setRating={setRating}/>
-    </Layout>
+    </>
   )
 }
+
+export default withLayout(Home)
