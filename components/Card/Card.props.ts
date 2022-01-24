57cf36585a1e8193f654495baa39b695e437ff1d
Radik Abubjazov <abubjazov@mail.ru>
2022-01-24 20:36:33 +0300

    components/Card: add layout

--- components/Card/Card.props.ts
+++ components/Card/Card.props.ts
@@ -1,5 +1,6 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 
-export interface CardProps extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
+export interface CardProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+	color?: 'white' | 'blue'
 	children: ReactNode
 }
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -3,20 +3,14 @@ import cn from 'classnames'
 import { CardProps } from './Card.props'
 import styles from './Card.module.css'
 
-export const Card = ({children, className, ...props}: CardProps): JSX.Element => {
+export const Card = ({color = 'white', children, className, ...props}: CardProps): JSX.Element => {
 	return (
-		<>
-		
-		</>
-		// <p
-		// 	className={cn(styles.p, className, {
-		// 		[styles.s]: fontSz === 's',
-		// 		[styles.m]: fontSz === 'm',
-		// 		[styles.l]: fontSz === 'l'
-		// 	})}
-		// 	{...props}
-		// >
-		// 	{children}
-		// </p>
+		<div className={cn(styles.card, className, {
+			[styles.blue]: color === 'blue'
+		})}
+			{...props}
+		>
+			{children}
+		</div>
 	)
 }
