45321d58f792ab229ed5b95273095d9d1c31a2ce
Radik Abubjazov <abubjazov@mail.ru>
2022-01-24 22:09:20 +0300

    components/HhDataBlock: add layout

--- components/HhDataBlock/HhDataBlock.tsx
+++ components/HhDataBlock/HhDataBlock.tsx
@@ -4,12 +4,46 @@ import { Card } from '..'
 import styles from './HhDataBlock.module.css'
 import { HhDataBlockProps } from './HhDataBlock.props'
 
-export const HhDataBlock = ({ count }:HhDataBlockProps): JSX.Element => {
+import RateIcon from './rateStar.svg'
+
+export const HhDataBlock = ({ count, juniorSalary, middleSalary, seniorSalary }:HhDataBlockProps): JSX.Element => {
 	return (
 		<div className={styles.hh}>
-			<Card className={styles.card}>
+			<Card className={styles.count}>
 				<div className={styles.title}>Всего вакансий</div>
-				<div className={styles.count}>{count}</div>
+				<div className={styles.countValue}>{count}</div>
+			</Card>
+
+			<Card className={styles.salary}>
+				<div>
+					<div className={styles.title}>Начальный</div>
+					<div className={styles.salaryValue}>{juniorSalary}</div>
+					<div className={styles.rate}>
+						<RateIcon className={styles.filled}/>
+						<RateIcon />
+						<RateIcon />
+					</div>
+				</div>
+
+				<div>
+					<div className={styles.title}>Средний</div>
+					<div className={styles.salaryValue}>{middleSalary}</div>
+					<div className={styles.rate}>
+						<RateIcon className={styles.filled}/>
+						<RateIcon className={styles.filled}/>
+						<RateIcon />
+					</div>
+				</div>
+
+				<div>
+					<div className={styles.title}>Профессионал</div>
+					<div className={styles.salaryValue}>{seniorSalary}</div>
+					<div className={styles.rate}>
+						<RateIcon className={styles.filled}/>
+						<RateIcon className={styles.filled}/>
+						<RateIcon className={styles.filled}/>
+					</div>
+				</div>
 			</Card>
 		</div>
 	)
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,9 +1,28 @@
+import { Card, HhDataBlock, Htag, Tag } from '../../components'
 import { TopPageComponentProps } from './TopPageComponent.props'
+
+import styles from './TopPageComponent.module.css'
+import { nanoid } from 'nanoid'
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
 	return (
-		<>
-			{products && products.length}
-		</>
+		<div className={styles.wrapper}>
+			<div className={styles.title}>
+				<Htag tag='h1'>{page.title}</Htag>
+				{products && <Tag color='grey' size='m'>{products.length}</Tag>}
+				<span>Сортировка</span>
+			</div>
+
+			<div>
+				{products && products.map(product => (<div key={nanoid()}>{product.title}</div>))}
+			</div>
+
+			<div className={styles.hhTitle}>
+				<Htag tag='h2'>Вакансии - {page.category}</Htag>
+				<Tag color='red' size='m'>hh.ru</Tag>
+			</div>
+
+			<HhDataBlock {...page.hh}/>
+		</div>
 	)
 }
