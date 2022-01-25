aa476263d6ddd28ad3b0204a4aa3588268fc5dd6
Radik Abubjazov <abubjazov@mail.ru>
2022-01-25 16:47:02 +0300

    components/: add Sort

---
+++ components/Sort/Sort.tsx
@@ -0,0 +1,11 @@
+import cn from 'classnames'
+
+import { SortProps } from './Sort.props'
+import styles from './Sort.module.css'
+
+export const Sort = ({}: SortProps): JSX.Element => {
+	return (
+		<>
+		</>
+	)
+}
