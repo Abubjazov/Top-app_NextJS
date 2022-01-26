2242abd6e1856ad67b0f876f167b8df69ed8df41
Radik Abubjazov <abubjazov@mail.ru>
2022-01-26 18:02:52 +0300

    components/Product: add css

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -4,16 +4,31 @@ import { nanoid } from 'nanoid'
 import { ProductProps } from './Product.props'
 import styles from './Product.module.css'
 import { Button, Card, Rating, Tag } from '..'
+import { priceRu } from '../../helpers/helper'
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
 		<Card className={styles.product} >
 			<div className={styles.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} /></div>
 			<div className={styles.title}>{product.title}</div>
-			<div className={styles.price}>{product.price}</div>
-			<div className={styles.credit}>{product.credit}</div>
-			<div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating}/></div>
-			<div className={styles.tags}>{product.categories.map(category => <Tag key={nanoid()} color={'ghost'}>{category}</Tag>)}</div>
+
+			<div className={styles.price}>
+				{priceRu(product.price)}
+				{product.oldPrice && <Tag className={styles.oldPrice} color={'green'}>{priceRu(product.price - product.oldPrice)}</Tag>}
+			</div>
+
+			<div className={styles.credit}>
+				{priceRu(product.credit)}/<span>мес</span>
+			</div>
+
+			<div className={styles.rating}>
+				<Rating rating={product.reviewAvg ?? product.initialRating}/>
+			</div>
+
+			<div className={styles.tags}>
+				{product.categories.map(category => <Tag key={nanoid()} color={'ghost'}>{category}</Tag>)}
+			</div>
+
 			<div className={styles.priseTitle}>цена</div>
 			<div className={styles.creditTitle}>в кредит</div>
 			<div className={styles.ratingTitle}>{product.reviewCount} отзывов</div>
@@ -22,6 +37,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 
 			<div className={styles.description}>{product.description}</div>
 			<div className={styles.features}>FEATURES</div>
+
 			<div className={styles.advBlock}>
 				<div className={styles.advantages}>
 					<div>Преимущества</div>
