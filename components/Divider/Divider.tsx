42bda3025a73dd457bb76b1dd51e02e6462b202c
Radik Abubjazov <abubjazov@mail.ru>
2022-01-26 18:05:58 +0300

    components/: add Divider

---
+++ components/Divider/Divider.tsx
@@ -0,0 +1,12 @@
+import cn from 'classnames'
+
+import {DividerProps } from './Divider.props'
+import styles from './Divider.module.css'
+
+export const Divider = ({className, ...props}: DividerProps): JSX.Element => {
+	return (
+		<>
+		
+		</>
+	)
+}
