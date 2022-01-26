1f69c81307bafac1b74c1d981d50858fedd267f6
Radik Abubjazov <abubjazov@mail.ru>
2022-01-26 14:16:00 +0300

    components/: add Product

---
+++ components/Product/Product.tsx
@@ -0,0 +1,12 @@
+import cn from 'classnames'
+
+import { ProductProps } from './Product.props'
+import styles from './Product.module.css'
+
+export const Product = ({className, ...props}: ProductProps): JSX.Element => {
+	return (
+		<>
+		
+		</>
+	)
+}
