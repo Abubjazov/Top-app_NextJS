38a204881201298294f12ad8bb5890f2acf54a22
Radik Abubjazov <abubjazov@mail.ru>
2022-01-26 22:33:28 +0300

    components/Product: add Image(Next)

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -5,11 +5,21 @@ import { ProductProps } from './Product.props'
 import styles from './Product.module.css'
 import { Button, Card, Divider, Rating, Tag } from '..'
 import { declOfNum, priceRu } from '../../helpers/helper'
+import Image from 'next/image'
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
 		<Card className={styles.product} >
-			<div className={styles.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} /></div>
+			<div className={styles.logo}>
+				<Image 
+					src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+					alt={product.title}
+					width={70}
+					height={70}
+				/>
+				{/* <img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} /> */}
+			</div>
+
 			<div className={styles.title}>{product.title}</div>
 
 			<div className={styles.price}>
