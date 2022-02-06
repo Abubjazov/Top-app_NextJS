2231128f4b83676ac53d55780091b9cfbf7c55a1
Radik Abubjazov <abubjazov@mail.ru>
2022-02-06 16:30:01 +0300

    proj: add layout/Header

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -3,8 +3,17 @@ import cn from 'classnames'
 import { HeaderProps } from './Header.props'
 import styles from './Header.module.css'
 
-export const Header = ({ ...props }: HeaderProps): JSX.Element => {
+import Logo from '../logo.svg' 
+import { IButton } from '../../components'
+
+export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
 	return (
-		<div {...props}>Header</div>
+		<header 
+			className={cn(className, styles.header)} 
+			{...props}
+		>
+			<Logo />
+			<IButton appearance='white' icon='MenuIcon'/>
+		</header>
 	)
 }
