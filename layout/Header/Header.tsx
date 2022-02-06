e5d60a145acd4fcb7340b4c1542e595067b5de3b
Radik Abubjazov <abubjazov@mail.ru>
2022-02-06 16:39:00 +0300

    layout/Header: add content

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,10 +1,13 @@
 import cn from 'classnames'
+import { motion } from 'framer-motion'
+
+import { IButton } from '../../components'
+import { Sidebar } from '../Sidebar/Sidebar'
 
 import { HeaderProps } from './Header.props'
 import styles from './Header.module.css'
 
 import Logo from '../logo.svg' 
-import { IButton } from '../../components'
 
 export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
 	return (
@@ -14,6 +17,11 @@ export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
 		>
 			<Logo />
 			<IButton appearance='white' icon='MenuIcon'/>
+
+			<motion.div className={styles.mobileMenu}>
+				<Sidebar />
+				<IButton className={styles.closeMenu} appearance='white' icon='CloseIcon'/>
+			</motion.div>
 		</header>
 	)
 }
