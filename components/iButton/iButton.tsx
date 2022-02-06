bc230f7afb91e2b778ad9acb408b1640380f56e8
Radik Abubjazov <abubjazov@mail.ru>
2022-02-06 15:46:21 +0300

    components/IButton: updated

--- components/iButton/iButton.tsx
+++ components/iButton/iButton.tsx
@@ -1,7 +1,7 @@
 import cn from 'classnames'
 
-import { IButtonProps, icons } from './iButton.props'
-import styles from './iButton.module.css'
+import { IButtonProps, icons } from './IButton.props'
+import styles from './IButton.module.css'
 
 export const IButton = ({appearance, className, icon, ...props}: IButtonProps): JSX.Element => {
 	const IconComponent = icons[icon]
