8c70d84adf78cc76c5b60700e23e7e1aad77d808
radikAbubjazov <abubjazov@mail.ru>
2022-02-10 21:52:57 +0300

    Lighthouse: accessibility+

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -8,8 +8,8 @@ export const Footer = ({className, ...props }: FooterProps): JSX.Element => {
 	return (
 		<footer className={cn(className, styles.footer)} {...props}>
 			<div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
-			<a href="http://" target="_blank" rel="noopener noreferrer">Пользовательское соглашение</a>
-			<a href="http://" target="_blank" rel="noopener noreferrer">Политика конфиденциальности</a>
+			<a href="https://github.com/Abubjazov" target="_blank" rel="noopener noreferrer">Пользовательское соглашение</a>
+			<a href="https://github.com/Abubjazov" target="_blank" rel="noopener noreferrer">Политика конфиденциальности</a>
 		</footer>
 	)
 }
